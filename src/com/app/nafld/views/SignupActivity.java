5fb5b85236645ff0761536216eff546bde4fd11c
rachnakhokhar <rachnakhokhar@gmail.com>
2014-06-30 00:27:13 +0530

    Added new signup activity

--- src/com/app/nafld/views/SignupActivity.java
+++ src/com/app/nafld/views/SignupActivity.java
@@ -2,57 +2,43 @@ package com.app.nafld.views;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.text.method.PasswordTransformationMethod;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.app.nafld.R;
 
-public class LoginActivity extends ActionBarActivity {
+public class SignupActivity extends ActionBarActivity{
 
+	private TextView btn_submit;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_signup);
 
-		setContentView(R.layout.activity_login);
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 
-		EditText password = (EditText) findViewById(R.id.edittext_password);
-		password.setTypeface(Typeface.SERIF);
-		password.setTransformationMethod(new PasswordTransformationMethod());
-
-		Button btn_signin = (Button) findViewById(R.id.btn_login);
-		btn_signin.setOnClickListener(new OnClickListener() {
+		btn_submit = (TextView) findViewById(R.id.btn_submit);
+		btn_submit.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View view) {
 				Intent screenChangeIntent = null;
-				screenChangeIntent = new Intent(LoginActivity.this,
+				screenChangeIntent = new Intent(SignupActivity.this,
 						ConditionsActivity.class);
-				LoginActivity.this.startActivity(screenChangeIntent);
+				SignupActivity.this.startActivity(screenChangeIntent);
 			}
 		});
 
-		// Action on click of Create An Account Button
-		TextView textview_createaccount = (TextView) findViewById(R.id.textview_createaccount);
-		textview_createaccount.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				Intent screenChangeIntent = null;
-				screenChangeIntent = new Intent(LoginActivity.this,
-						SignupActivity.class);
-				LoginActivity.this.startActivity(screenChangeIntent);
-			}
-		});
 	}
 
 	@Override
@@ -77,5 +63,4 @@ public class LoginActivity extends ActionBarActivity {
 		}
 		return ret;
 	}
-
 }
